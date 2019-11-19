bbf34ab72234d729503c0cddfad9c8659f9b684c
Jenney Le <jenneyle@Jenneys-MacBook-Air.local>
2019-11-19 17:03:21 +1100

    facts

--- app/src/main/java/com/example/ck/Dashboard.java
+++ app/src/main/java/com/example/ck/Dashboard.java
@@ -41,17 +41,25 @@ public class Dashboard extends AppCompatActivity {
 
     public void showRandomFact(){
         shuffleFacts();
-        d_countryname.setText(factArray[0].getDash_country_name());
-        d_countryfact.setText(factArray[0].getDash_country_fact());
+        d_countryname.setText(factArray[1].getDash_country_name());
+        d_countryfact.setText(factArray[1].getDash_country_fact());
     }
 
-    Facts f01 = new Facts("Freeya", 1, "is cool");
-    Facts f02 = new Facts("Korea", 2, "is fun");
-    Facts f03 = new Facts("ko", 3, "is ");
-    Facts f04 = new Facts("sghs", 4, "isighsdg");
+    Facts f01 = new Facts("France", 1, "No pig is allowed to be called Napolean in France!");
+    Facts f02 = new Facts("Greenland", 2, "Greenland is actually the world's biggest island - by area - that is not a continent");
+    Facts f03 = new Facts("Egypt", 3, "The Pyramid of Giza is the oldest of the wonders of the Ancient world, that is still in existence");
+    Facts f04 = new Facts("Papua New Guinea", 4, "Papua New Guinea is the most linguistically diverse country in the world with 851 individual languages listed ");
+    Facts f05 = new Facts("Greece", 5, "The official name of Greece is the Hellenic Republic");
+    Facts f06 = new Facts("China", 6, "More than 10million people visit the Great Wall of China every year");
+    Facts f07 = new Facts("India", 7, "The national symbol of India is the endangered Benegal Tiger");
+    Facts f08 = new Facts("Mexico", 8, "Mexico is hoe to over 30 UNESCO Wolrd Heritage Sites");
+    Facts f09 = new Facts("Venezuela", 4, "Venezuela is home to the world's tallest waterfall - Angel Falls");
+    Facts f10 = new Facts("Chile", 4, "The national sport of Chile is Rodeo which is still very popular in rural areas of the country");
+
+
 
     Facts [] factArray = new Facts[]{
-            f01, f02, f03, f04
+            f01, f02, f03, f04, f05, f06, f07, f08, f09,f10
     };
 
     public void shuffleFacts(){
